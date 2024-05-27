23435a205cb9f64e03dabbbc8658131063ed504c
rekebuba <abukiabdullahi1@gmail.com>
2024-05-27 23:53:21 +0300

    AirBnB clone - RESTful API

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -25,6 +25,41 @@ def amenityInPlace(place_id):
 
     result = []
     for amenity in place.amenities:
-        result.append(BaseModel.to_dict(amenity))
+        result.append(amenity.to_dict())
 
     return jsonify(result)
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',  methods=['DELETE'])
+def deleteAmenity(place_id, amenity_id):
+    """Deletes a Amenity object by a Place id"""
+    if not storage.get(Place, place_id) or not storage.get(Amenity, amenity_id):
+        abort(404)
+
+    place = storage.get(Place, place_id)
+
+    result = []
+    for amenity in place.amenities:
+        if amenity.id == amenity_id:
+            storage.delete(amenity)
+
+    storage.save()
+
+    return make_response(jsonify({}), 200)
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',  methods=['POST'])
+def linkAmenity(place_id, amenity_id):
+    """Link a Amenity object to a Place"""
+    if not storage.get(Place, place_id) or not storage.get(Amenity, amenity_id):
+        abort(404)
+
+    place = storage.get(Place, place_id)
+
+    result = []
+    for amenity in place.amenities:
+        if amenity.id == amenity_id:
+            storage.delete(amenity)
+
+    storage.save()
+
+    return make_response(jsonify({}), 200)
